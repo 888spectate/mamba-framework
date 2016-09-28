74c64a2c996b82008d719962543aaa2c54a1ed3a
Mikhail Medvedev <moorcock84@gmail.com>
2016-09-28 12:18:23 +0100

    Moving storm log to syslog

--- mamba/utils/logger.py
+++ mamba/utils/logger.py
@@ -5,15 +5,31 @@
 """
 Mamba logger utilities
 """
-import os
+import time
+import syslog as std_syslog
 
-from twisted.python import logfile
+# from twisted.python import logfile
+from twisted.python import syslog
 from storm.tracer import debug as storm_debug
 
 
-class StormDebugLogFile(logfile.DailyLogFile):
+class StormDebugLogFile(object):
     """Log storm traces to the log directory
     """
+    def __init__(self):
+        self._observer = syslog.SyslogObserver(
+            'storm_debug', syslog.DEFAULT_OPTIONS, std_syslog.LOG_DEBUG
+        )
+
+    def write(self, msg):
+        self._observer.emit({
+            'message': str(msg),
+            'time': time.time(),
+            'isError': 0
+        })
+
+    def flush(self):
+        pass
 
     @classmethod
     def start(cls):
@@ -21,14 +37,15 @@ class StormDebugLogFile(logfile.DailyLogFile):
         """
         # Avoiding circular import
         from mamba.utils import config
-        settings = config.Application('config/application.json')
-        log_dir = getattr(settings, 'log_dir', 'logs')
-        if log_dir == 'logs':
-            log_file = 'storm.log'
-        else:
-            log_file = '{}-storm.log'.format(getattr(settings, 'name'))
-        obj = cls.fromFullPath(os.path.join(log_dir, log_file))
-        storm_debug(True, stream=obj)
+        # settings = config.Application('config/application.json')
+        # log_dir = getattr(settings, 'log_dir', 'logs')
+        # if log_dir == 'logs':
+        #     log_file = 'storm.log'
+        # else:
+        #     log_file = '{}-storm.log'.format(getattr(settings, 'name'))
+        # obj = cls.fromFullPath(os.path.join(log_dir, log_file))
+
+        storm_debug(True, stream=cls())
 
     @staticmethod
     def stop():
